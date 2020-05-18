a988a859d1fc1b8260b2c235cd3a56020ff8113f
Michael Wilson <nosliw.michael@gmail.com>
2020-05-17 21:40:06 -0500

    Added In-Memory DB for User Service.
    Added H2 in-memory database to test user service.
    Changed user service properties to allow it to be run independent of
    config and discovery service. Can now be fully tested by itself.
    Added sql scripts for H2 db.
    Removed configuration and executions from spring-boot-maven-plugin in
    all projects. Removed this plugin from microservice pom.
    Setup JPA in User service.
    Added a getAllUsers method for User service.
    Moved type conversion from UserService to UserController.

--- microservice/user/user-api/src/main/java/com/aperture/user/endpoint/UserEndpoint.java
+++ microservice/user/user-api/src/main/java/com/aperture/user/endpoint/UserEndpoint.java
@@ -1,5 +1,7 @@
 package com.aperture.user.endpoint;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.aperture.user.remote.UserRemote;
 
 @RestController
-@RequestMapping(value="/user-api")
+@RequestMapping(value = "/user-api")
 public interface UserEndpoint {
-	
-	@GetMapping(value="/user/{userId}")
+
+	@GetMapping(value = "/user/")
+	public List<UserRemote> getAllUsers();
+
+	@GetMapping(value = "/user/{userId}")
 	UserRemote getUserById(@PathVariable("userId") Long userId);
 
 }
